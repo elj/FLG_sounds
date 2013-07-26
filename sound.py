6ac5623a9133930acd9442e4e6d1eb047d988cef
deleted <pi@raspberrypi.(none)>
2013-07-25 23:09:07 -0700

    Added the scraper.

--- sound.py
+++ sound.py
@@ -26,7 +26,10 @@ def median(mylist):
 
 def play_sound(filename, speed=1.0, block=False):
     filename = os.path.join(SOUND_ROOT, filename)
-    p = subprocess.Popen(('play',filename, 'tempo', str(speed)), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    assert os.path.exists(filename)
+    out = open('/dev/null', 'w')
+    #out = None
+    p = subprocess.Popen(('play',filename, 'tempo', str(speed)), stdout=out, stderr=out)
     if block:
         p.wait()
 
@@ -59,19 +62,32 @@ class Breather(threading.Thread):
         while True:
             while not self.queue.empty():
                 self.speed = self.queue.get()
-            print "Breathing speed: %f" % self.speed
+            #print "Breathing speed: %f" % self.speed
             play_sound(breathing_sounds.next(), speed=self.speed, block=True)
 
     @classmethod
     def counter_to_speed(klass, counter):
         return float(counter.value) / float(counter.max_value) * (MAX_BREATH_SPEED - MIN_BREATH_SPEED) + MIN_BREATH_SPEED
 
+class Looper(threading.Thread):
+    def __init__(self, soundfile, speed=1.0, *args, **kwargs):
+        super(Looper, self).__init__(*args, **kwargs)
+        self.daemon = True
+        self.speed = speed
+        self.soundfile = soundfile
+
+    def run(self):
+        while True:
+            play_sound(self.soundfile, speed=self.speed, block=True)
+
 class ActivityCounter(object):
     def __init__(self, max_value=60, growth_limit=0.0003, decay_rate=1.0):
         self.value = 0
         self.max_value = max_value
+
         self.decay_rate = decay_rate # in seconds
         self.growth_limit = growth_limit # in seconds
+
         self.last_decay_time = time.time()
         self.last_grow_time = time.time()
 
@@ -114,7 +130,7 @@ class IRSensor(object):
             while delta > 0:
                 self.counter += 1
                 delta -= 0.01
-        print "IR %d delta: %f" % (self.pin, self.value - self.prior_value)
+        #print "IR %d delta: %f" % (self.pin, self.value - self.prior_value)
 
 
 def get_breath_speed(counter):
@@ -144,8 +160,11 @@ if __name__ == '__main__':
     speedqueue = Queue()
     breather = Breather(speedqueue)
     breather.start()
-    counter = ActivityCounter()
 
+    scraper = Looper('normalized/scrapes_loop.wav')
+    scraper.start()
+    
+    counter = ActivityCounter()
     ir_sensors = [ IRSensor(pin, counter) for pin in IR_PINS  ]
     
     speed = 0.8
@@ -153,6 +172,6 @@ if __name__ == '__main__':
         for irs in ir_sensors:
             irs.update()
         counter.update()
-        print("Speed: %f" % Breather.counter_to_speed(counter))
+        #print("Speed: %f" % Breather.counter_to_speed(counter))
         speedqueue.put(Breather.counter_to_speed(counter))
         time.sleep(0.05)
